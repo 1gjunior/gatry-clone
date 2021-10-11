6227239f3e604446961f947e7ec566e4d79dc1d3
Gilberto Junior <63628013+1gjunior@users.noreply.github.com>
2021-10-11 16:01:25 -0300

    Feature 5 (#2)
    * feat: :sparkles: added absolute imports config
    
    * feat: :sparkles: integrated react-router

---
+++ src/pages/Root.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PagesPromotionSearch from './Promotion/Search/Search';
+import PagesPromotionForm from './Promotion/Form/Form';
+
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create" component={PagesPromotionForm} />
+        <Route path="/edit/:id" component={PagesPromotionForm} />
+        <Route path="/" component={PagesPromotionSearch} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Root;
