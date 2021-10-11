bb9ec173f99687ab5a4b34edbcf9b00b9c68e088
Gilberto Junior <junior.siilva182@gmail.com>
2021-10-10 23:32:38 -0300

    feat: :sparkles: integrated react-router

---
+++ src/pages/Promotion/Form/Form.js
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+const PagesPromotionForm = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      FORM
+      {id && <div>id: {id}</div>}
+    </div>
+  );
+};
+
+export default PagesPromotionForm;
